14da2e4d26e5afa2bf501b157c1ad2efdf29d744
NICOLAS MAEKE <NICOLAS-MAEKE@gmx.de>
2018-09-04 20:27:15 +0200

    30000 savings

--- Masterarbeit/src/start/StartAfterInitialSolution.java
+++ Masterarbeit/src/start/StartAfterInitialSolution.java
@@ -8,7 +8,7 @@ public class StartAfterInitialSolution {
 
 	public static void main(String[] args) {
 
-		ReadInitialSolution data = new ReadInitialSolution("/Users/nicolasmaeke/gitproject/masterarbeit/Masterarbeit/data/initial/full_sample_real_867_SF_207_stoppoints_initialloesung_1Depot.txt");
+		ReadInitialSolution data = new ReadInitialSolution("C:\\Users\\Nicolas\\git\\masterarbeit\\Masterarbeit\\data\\initial\\full_sample_real_867_SF_207_stoppoints_initialloesung_5Depot.txt");
 
 		//data.printInitialSolution();
 		data.defineDepotCapacity();
@@ -16,7 +16,7 @@ public class StartAfterInitialSolution {
 		
 		VariableNeighborhoodSearch improvement = new VariableNeighborhoodSearch(data);
 
-		improvement.startVNS(20000, 50, "/Users/nicolasmaeke/gitproject/masterarbeit/Masterarbeit/data/solution/full_sample_real_867_SF_207_stoppoints_solution_1Depot.txt");
+		improvement.startVNS(20000, 50, "C:\\Users\\Nicolas\\git\\masterarbeit\\Masterarbeit\\data\\solution\\full_sample_real_867_SF_207_stoppoints_solution_5Depot.txt");
 
 		/**
 		VNSwoShaking improvement = new VNSwoShaking(data);
